e0c204ca0e0743861093d72c91ad474376601fc1
Matthew Vandenberg <mv5903@gmail.com>
2022-01-20 15:23:30 -0500

    add two blocks

---
+++ mattmod/src/main/java/net/kwikmatt/mattmod/TutorialMod.java
@@ -0,0 +1,38 @@
+package net.kwikmatt.mattmod;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.kwikmatt.mattmod.blocks.*;
+
+public class TutorialMod implements ModInitializer {
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger("mattmod");
+	public static final MattBlock[] blocks =
+	{
+			new MattBlock("blocktest"),
+			new MattBlock("testblock")
+	};
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution
+		for (MattBlock block: blocks) {
+			block.addToGame();
+		}
+		LOGGER.info("Hello Fabric world!");
+	}
+}
